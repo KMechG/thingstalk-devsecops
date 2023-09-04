ea3b5cdd84c041bc48a3a6a09f8f700f8648ee10
karima mechergui <karima.mechergui@machinestalk.com>
2023-09-04 14:40:42 +0100

    add file cmd commit sign

--- commitsign.sh
+++ commitsign.sh
@@ -1,7 +1,6 @@
-
-#/bin/sh
+#!/bin/bash
 gpg --version
 gpg --full-generate-key
-git config --global user.signingkey $0
+git config --global user.signingkey <id>
 git config --global commit.gpgsign true
 git log --show-signature
