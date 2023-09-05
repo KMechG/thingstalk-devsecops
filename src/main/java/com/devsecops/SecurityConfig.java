6fb165084ab26c9401d8a860d9444c1455a55fb3
karima mechergui <karima.mechergui@machinestalk.com>
2023-09-05 14:31:21 +0100

    add security dependency

--- src/main/java/com/devsecops/SecurityConfig.java
+++ src/main/java/com/devsecops/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.devsecops;
 
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
