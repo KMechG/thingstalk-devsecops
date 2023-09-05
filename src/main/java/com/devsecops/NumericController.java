bd446d436213f7dce867893fe3800ded0f2582aa
karima mechergui <karima.mechergui@machinestalk.com>
2023-09-05 16:30:55 +0100

    add security dependency

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -1,8 +1,6 @@
 package com.devsecops;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-//import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +9,8 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class NumericController {
 
-	//private final Logger logger = LoggerFactory.getLogger(getClass());
-	//private static final String baseURL = "http://node-service:5000/plusone";
-	//private static final String baseURL = "http://localhost:5000/plusone";
+	
+	
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
@@ -38,11 +35,7 @@ public class NumericController {
 
 		@GetMapping("/increment/{value}")
 		public int increment(@PathVariable int value) {
-			//ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
-			//String response = responseEntity.getBody();
-			//logger.info("Value Received in Request - " + value);
-			/*logger.info("Node Service Response - " + response);
-			return Integer.parseInt(response);*/
+			
 			return 1;
 		}
 	}
