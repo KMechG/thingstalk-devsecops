f4b74fb544cafbf783c9aaf01fa3fa0f35997c92
Karima Mechergui <89023462+KMechG@users.noreply.github.com>
2023-10-03 16:18:59 +0100

    Update NumericController.java

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -2,17 +2,23 @@ package com.devsecops;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.boot.web.client.RestTemplateBuilder;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class NumericController {
 
-	//private final Logger logger = LoggerFactory.getLogger(getClass());
-	//private static final String baseURL = "http://tb-core-svc:5000/plusone";
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private static final String baseURL = "http://node-service:5000/plusone";
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
@@ -35,7 +41,14 @@ public class NumericController {
 			return message;
 		}
 
-		
+		@GetMapping("/increment/{value}")
+		public int increment(@PathVariable int value) {
+			ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
+			String response = responseEntity.getBody();
+			logger.info("Value Received in Request - " + value);
+			logger.info("Node Service Response - " + response);
+			return Integer.parseInt(response);
+		}
 	}
 
 }
